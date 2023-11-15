1eba672dd57c8bd439b09f0c3465cf31ac1e4ef0
Nzeru Creations <93992299+FrancisZoeChituwi@users.noreply.github.com>
2023-11-15 05:16:06 -0800

    GUI Built

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 # Graphical Calculator with Tkinker
+import tkinter as tk
+calculation =""
+
+def add_calculation(symbol):
+    pass
+
+def evaluate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+# GUI Built
+root = tk.Tk()
+root.geometry("400x250")
+root.mainloop()
